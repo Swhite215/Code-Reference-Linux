437d273d6e9672f0b001819813a2767dd0ab42c5
Spencer White <swhit210@ford.com>
2020-12-23 16:41:14 -0500

    added umask

--- bash/chmod.sh
+++ bash/chmod.sh
@@ -16,3 +16,20 @@ chmod 111 myFile # All groups only execute permissions
 chmod 555 myFile # All groups read and execute permissions
 chmod 333 myFile # All groups write and execute permissions
 chmod 000 myFIle # All groups no permissions
+
+id # displays user identity
+umask 1234 # set default file permissions - 
+        # 1 - special permissions
+        # 2 - user/owner
+        # 3 - group
+        # 4 - others
+    # 0 = any permission may be set (read, write, execute)
+    # 1 = setting of execute permission is prohibited (read and write)
+    # 2 = setting of write permission is prohibited (read and execute)
+    # 3 = setting of write and execute permission is prohibited (read only)
+    # 4 = setting of read permission is prohibited (write and execute)
+    # 5 = setting of read and execute permission is prohibited (write only)
+    # 6 = setting of read and write permission is prohibited (execute only)
+    # 7 = all permissions are prohibited from being set (no permissions)
+chgrp # change a file's group ownership
+passwd # change password
