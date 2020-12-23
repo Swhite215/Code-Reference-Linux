d2f8bcb6012d596e2c4f69aea9cf53cbf6339571
Spencer White <swhit210@ford.com>
2020-12-23 16:42:24 -0500

    added storage commands

---
+++ bash/storage.sh
@@ -0,0 +1,11 @@
+# Example commands for storage management
+
+mount # mount a filesystem
+umount # unmoutn a filesystem
+fsck # check and repair a file system
+fdisk # manipulate disk partition table
+mkfs # create a file system
+dd # convert and copy a file
+genisoimage # cerate an ISO 9660 image file\
+wodim # write data to optical storage media
+md5sum # calculate an MD5 checksum
