550ab02f33bfbd8f2526b0d0ba719ebab118735a
Spencer White <swhit210@ford.com>
2020-12-23 16:41:44 -0500

    added ping, traceroute, ip and netstat

---
+++ bash/networking.sh
@@ -0,0 +1,6 @@
+# Example commands for networking
+
+ping # send an ICMP ECHO_REQUEST to network hosts
+traceroute # print the route packetts trace to a network host
+ip # show/manipulate routing, devices, policy routing, and tunnels
+netstat # print network connections, routing tables,, interface statistics, masquerade connections and multi-cast memberships
